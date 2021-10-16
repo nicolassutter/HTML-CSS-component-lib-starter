919dbccfb8dd36659369953b71e23868b4b23bc0
Nicolas Sutter <sutter.nicolas81@gmail.com>
2021-10-16 14:10:25 +0200

    Meileure perfs tailwiind -> jit

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,8 @@ module.exports = {
     './components/**/*.ejs',
     'index.ejs'
   ],
+  /** Important pour la perf !! */
+  mode: 'jit',
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
