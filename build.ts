b62a412a0ddc5855aa5b887deaa85b94467f383e
Nicolas Sutter <contact.sutter.nicolas@gmail.com>
2021-10-20 09:56:57 +0200

    Simplification récursivité build des composants

--- build.ts
+++ build.ts
@@ -23,13 +23,6 @@ const get_all_files = (path: string): Promise<string[]> => {
 }
 
 /**
- * Créé le dossier dist et dist/components
- */
-const create_dir = async () => {
-  return promises.mkdir(destination)
-}
-
-/**
  * Supprimme le dossier dist
  * On retourne toujours une promise
  */
@@ -55,18 +48,38 @@ const get_html = (path: string) => ejs.renderFile(path, { rmWhitespace: true })
 const write_file = (file: string, content: string) => promises.writeFile(file, content)
 
 /**
- * Génère tout le contenu HTML de chaque composant
- * @returns {Array.<{ path: string, content: string }>} Tableau d'objets contenant le chemin d'écriture + le contenu
+ * Génère tout le contenu HTML de chaque composant + créé tous le dossiers nécéssaires
+ *
+ * @returns {Array.<{ path: string, content: string }>} Tableau d'objets contenant le chemin d'écriture du fichier + le contenu
  */
-const render_files = async () => {
+const process_files = async () => {
   const test = await get_all_files('./components')
 
   /** On n'inclus que les composants eux mêmes et pas leurs parties */
-  const comps = test.filter(name => !name.includes('/parts/'))
+  const comps = test
+    .filter(name => !name.includes('/parts/'))
+    .map(component => {
+      /**
+       * On récupère le dossier dans lequel on écrira le fichier
+       */
+      const [parent_directory] = component.split('.ejs')
+      const path = parent_directory.split('/').slice(0, -1).join('/')
+
+      return { component, path }
+    })
 
   const files = []
 
-  for await (const file of comps) {
+  for await (const { component: file, path: folder } of comps) {
+    /**
+     * On tente la création du dossier contenant le fichier
+     */
+    try {
+      await promises.mkdir(`./dist/${folder}`, { recursive: true })
+    } catch (error) {
+      console.log(chalk.yellow(error))
+    }
+
     const [path] = file.split('.')
     const newName = `./dist/${path}.html`
 
@@ -102,8 +115,7 @@ const write_files = async (files: Files) => {
  */
 const build = () => {
   return remove_dir()
-    .then(() => create_dir())
-    .then(() => render_files())
+    .then(() => process_files())
     .then((files: Files) => write_files(files))
     .then(() => 'done')
     .catch(() => console.log(chalk.red.bold('Une erreur de build est survenue')))
